9cd34eda4c5718d117d1a8a7d7e4b6e133a863d5
sukhanov16 <ppsukhanov@gmail.com>
2024-09-23 14:54:35 +0530

    first commit

---
+++ sukhanov.py
@@ -0,0 +1,22 @@
+import math
+
+
+example_variable = math.sqrt
+power_variable = math.pow(2,5)
+ceil_variable = math.ceil(6.3)
+floor_variable = math.floor(5.5)
+fabs_variables = math.fabs(-9)
+factorial_variable = math.factorial(4)
+sin_variable = math.sin(math.pi/2)
+cos_variable = math.cos(math.pi/2)
+tan_variable = math.tan(math.pi)
+log2_variable = math.log2(3)
+log10_variable = math.log10(3)
+degree_variable = math.degrees(math.pi/2)
+radian_variable = math.radians(90)
+euler_variable = math.e
+infinity_variable = math.inf
+nan_variable = math.nan
+gcd_variable = math.gcd(4,5)
+lcm_variable = math.lcm(3,4)
+print(lcm_variable)
